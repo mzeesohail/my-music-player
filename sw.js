8e04d83278799246a3e9284397639f84b280405a
mzeesohail <zeeshan1074s@gmail.com>
2026-09-25 02:06:19 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,52 @@
+```javascript
+const CACHE_NAME = "my-music-player-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(APP_FILES);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  const request = event.request;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  const url = new URL(request.url);
+
+  // Only cache files belonging to this GitHub Pages app.
+  if (url.origin === location.origin) {
+    event.respondWith(
+      caches.match(request).then(cachedResponse => {
+        return cachedResponse || fetch(request);
+      })
+    );
+  }
+});
+```
